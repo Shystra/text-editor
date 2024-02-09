07a79ff3d9a20808416062cfa1f8bf22d7ebd9a1
Shystra <jlsp.2019@hotmail.com>
2024-02-09 10:33:15 -0300

    just test in aside

--- src/components/tests/aside-nav.spec.tsx
+++ src/components/tests/aside-nav.spec.tsx
@@ -1,12 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { AsideNav } from '../aside-nav';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import "@testing-library/jest-dom";
 
-describe("AsideNav", () => {
-    it("showld render correctly", () => {
-        const queryClient = new QueryClient();
+const mockNavigate = jest.fn();
+jest.mock(
+    'react-router', () => ({
+        ...jest.requireActual('react-router'),
+        useNavigate: () => mockNavigate
+    })
+);
+
+const renderComponent = () => {
+    const queryClient = new QueryClient();
         render(
             <QueryClientProvider client={queryClient}>
                 <BrowserRouter>
@@ -15,7 +22,39 @@ describe("AsideNav", () => {
             </QueryClientProvider>
         )
 
+    return { queryClient }
+}
+
+describe("AsideNav", () => {
+    it("should render correctly", () => {
+        // const queryClient = new QueryClient();
+        // render(
+        //     <QueryClientProvider client={queryClient}>
+        //         <BrowserRouter>
+        //             <AsideNav />
+        //         </BrowserRouter>
+        //     </QueryClientProvider>
+        // )
+        renderComponent();
         expect(screen.getByText("All files")).toBeInTheDocument();
         expect(screen.getByText("Favorites")).toBeInTheDocument();
+    })
+
+    it("should call navigate when hits the button All Files", () => {
+        renderComponent();
+
+
+        const btnAllFiles = screen.getByText("All files");
+        fireEvent.click(btnAllFiles);
+        expect(mockNavigate).toHaveBeenCalledWith("/")
+    })
+
+
+    it("should call navigate when hits the button Favorites", () => {
+        renderComponent();
+
+        const btnFavorite = screen.getByText("Favorites");
+        fireEvent.click(btnFavorite);
+        expect(mockNavigate).toHaveBeenCalledWith("/favorites")
     })
 })
