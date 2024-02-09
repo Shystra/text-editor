5264c61018bb73c0bfdd17af0074ce9249058084
Shystra <jlsp.2019@hotmail.com>
2024-02-09 09:27:35 -0300

    just first test

--- src/components/tests/aside-nav.spec.tsx
+++ src/components/tests/aside-nav.spec.tsx
@@ -1,66 +1,21 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'
-import { BrowserRouter } from "react-router-dom";
-
+import { render, screen } from '@testing-library/react';
 import { AsideNav } from '../aside-nav';
-import { QueryClientProvider, QueryClient } from 'react-query';
-
-const mockNavigate = jest.fn();
-
-jest.mock('react-router', () => ({
-    ...jest.requireActual('react-router'),
-    useNavigate: () => mockNavigate
-}))
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import "@testing-library/jest-dom";
 
 describe("AsideNav", () => {
-    it("should render correctly", () => {
+    it("showld render correctly", () => {
         const queryClient = new QueryClient();
         render(
-            <BrowserRouter>
             <QueryClientProvider client={queryClient}>
-                <AsideNav />
+                <BrowserRouter>
+                    <AsideNav />
+                </BrowserRouter>
             </QueryClientProvider>
-            </BrowserRouter>
         )
 
         expect(screen.getByText("All files")).toBeInTheDocument();
         expect(screen.getByText("Favorites")).toBeInTheDocument();
-        expect(screen.getByText("New")).toBeInTheDocument();
-    });
-
-    it("should call navigate with correct params", () => {
-        const queryClient = new QueryClient();
-        render(
-            <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <AsideNav />
-            </QueryClientProvider>
-            </BrowserRouter>
-        )
-
-        const btn = screen.getByText("All files")
-
-        fireEvent.click(btn);
-
-        expect(mockNavigate).toHaveBeenCalled();
-        expect(mockNavigate).toHaveBeenCalledWith("/");
-    })
-
-    it.each([["All files", "/"], ["Favorites", "/favorites"]])("testings %s", (text, path) => {
-        const queryClient = new QueryClient();
-        render(
-            <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <AsideNav />
-            </QueryClientProvider>
-            </BrowserRouter>
-        )
-
-        const btn = screen.getByText(text);
-
-        fireEvent.click(btn);
-
-        expect(mockNavigate).toHaveBeenCalled();
-        expect(mockNavigate).toHaveBeenCalledWith(path);
     })
 })
